235861afce6b4194e182110da2e088c37d5a1fd5
8BitsPerByte <jonathan.westfall1@gmail.com>
2025-12-28 12:44:59 -0600

    Added Unity 6.4 GUID support for the new namespace. Unity moved GUID to the runtime assembly and it is no longer editor only.

--- Editor/Sprite Editor/Utilities/GUIDS/StringGUID.cs
+++ Editor/Sprite Editor/Utilities/GUIDS/StringGUID.cs
@@ -1,7 +1,8 @@
-using UnityEditor;
 
 namespace SFEditor.Utilities
 {
+	// In Unity 6.4 they removed the Editor only requirements for a lot of the GUID and type caching API.
+#if UNITY_6000_4_OR_NEWER
 	/// <summary>
 	/// Helper struct to serialize GUIDS with strings for any type of asset.
 	/// </summary>
@@ -10,12 +11,29 @@ namespace SFEditor.Utilities
     {
 		[UnityEngine.SerializeField] private string _stringGUID;
 
-		public StringGUID(GUID guid)
+		public StringGUID(UnityEngine.GUID guid)
+		{
+			_stringGUID = guid.ToString();
+		}
+		public static implicit operator UnityEngine.GUID(StringGUID data) => new UnityEngine.GUID(data._stringGUID);
+		public static implicit operator StringGUID(UnityEngine.GUID data) => new StringGUID(data);
+	}
+#else 
+	/// <summary>
+	/// Helper struct to serialize GUIDS with strings for any type of asset.
+	/// </summary>
+	[System.Serializable]
+	public struct StringGUID
+	{
+		[UnityEngine.SerializeField] private string _stringGUID;
+
+		public StringGUID(UnityEditor.GUID guid)
 		{
 			_stringGUID = guid.ToString();
 		}
 
-		public static implicit operator GUID(StringGUID data) => new GUID(data._stringGUID);
-		public static implicit operator StringGUID(GUID data) => new StringGUID(data);
+		public static implicit operator UnityEditor.GUID(StringGUID data) => new UnityEditor.GUID(data._stringGUID);
+		public static implicit operator StringGUID(UnityEditor.GUID data) => new StringGUID(data);
 	}
+#endif
 }
--- Editor/Sprite Editor/Utilities/GUIDS/StringGUIDList.cs
+++ Editor/Sprite Editor/Utilities/GUIDS/StringGUIDList.cs
@@ -2,21 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-using UnityEditor;
-
 namespace SFEditor.Utilities
 {
+	
+#if UNITY_6000_4_OR_NEWER
 	/// <summary>
 	/// A serialzed helper list for StringGUID that can be used to keep track for assets and objects.
 	/// <seealso cref="StringGUID"/>
 	/// </summary>
 	[Serializable]
-    public class StringGUIDList : IReadOnlyList<GUID>
+    public class StringGUIDList : IReadOnlyList<UnityEngine.GUID>
     {
 		[UnityEngine.SerializeField]
 		private List<StringGUID> _list = new List<StringGUID>();
 
-		GUID IReadOnlyList<GUID>.this[int index] 
+		UnityEngine.GUID IReadOnlyList<UnityEngine.GUID>.this[int index] 
 		{ 
 			get => _list[index];
 		}
@@ -26,7 +26,7 @@ namespace SFEditor.Utilities
 			set => _list[index] = value;
 		}
 
-		IEnumerator<GUID> IEnumerable<GUID>.GetEnumerator()
+		IEnumerator<UnityEngine.GUID> IEnumerable<UnityEngine.GUID>.GetEnumerator()
 		{
 			// Not yet used.
 			throw new NotImplementedException();
@@ -38,4 +38,39 @@ namespace SFEditor.Utilities
 		public void RemoveAt(int i) => _list.RemoveAt(i);
 		public void Add(StringGUID value) => _list.Add(value);
 	}
+	
+#else
+	/// <summary>
+	/// A serialzed helper list for StringGUID that can be used to keep track for assets and objects.
+	/// <seealso cref="StringGUID"/>
+	/// </summary>
+	[Serializable]
+	public class StringGUIDList : IReadOnlyList<UnityEditor.GUID>
+	{
+		[UnityEngine.SerializeField]
+		private List<StringGUID> _list = new List<StringGUID>();
+
+		UnityEditor.GUID IReadOnlyList<UnityEditor.GUID>.this[int index] 
+		{ 
+			get => _list[index];
+		}
+		public StringGUID this[int index]
+		{
+			get => _list[index];
+			set => _list[index] = value;
+		}
+
+		IEnumerator<UnityEditor.GUID> IEnumerable<UnityEditor.GUID>.GetEnumerator()
+		{
+			// Not yet used.
+			throw new NotImplementedException();
+		}
+
+		public int Count => _list.Count;
+		public IEnumerator GetEnumerator() => _list.GetEnumerator();
+		public void Clear() => _list.Clear();
+		public void RemoveAt(int i) => _list.RemoveAt(i);
+		public void Add(StringGUID value) => _list.Add(value);
+	}
+#endif
 }
