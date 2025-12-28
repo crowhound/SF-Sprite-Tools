eb771efab8f530037a82c095fd677ca9ca8e0206
8BitsPerByte <jonathan.westfall1@gmail.com>
2025-12-28 12:44:00 -0600

    Resimplified the SFNeighbourRuleTile  code after completing test to see what caused 6.4/6.5 to crash.
    It was related to the order of the SFSpriteTileShape recalculating tilemap bounds and causing a call to RefreshTiles when they were not ready. Technically this shouldn't cause a crash.
    
    Bug report to Unity was filed.

--- Runtime/Tiles/SFNeighbourRuleTile.cs
+++ Runtime/Tiles/SFNeighbourRuleTile.cs
@@ -1,20 +1,37 @@
+using System;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-[CreateAssetMenu(menuName = "2D/SF/Tiles/SFNeighbour Rule Tile", fileName = "Neighbor Rule Tile")]
-public class SFNeighbourRuleTile : RuleTile<SFNeighbourRuleTile.Neighbor> {
+namespace SF.TileModule
+{
+    [CreateAssetMenu(menuName = "2D/SF/Tiles/SFNeighbour Rule Tile", fileName = "Neighbor Rule Tile")]
+    public class SFNeighbourRuleTile : RuleTile<SFNeighbourRuleTile.Neighbor> {
 
-    public class Neighbor : RuleTile.TilingRule.Neighbor {
-        public const int Null = 3;
-        public const int NotNull = 4;
-    }
-
-    public override bool RuleMatch(int neighbor, TileBase tile) {
-
-        switch (neighbor) {
-            case Neighbor.Null: return tile == null;
-            case Neighbor.NotNull: return tile != null;
+        public class Neighbor : RuleTile.TilingRuleOutput.Neighbor {
+            public const int Null = 3;
+            public const int NotNull = 4;
         }
-        return base.RuleMatch(neighbor, tile);
+        
+        public override bool RuleMatch(int neighbor, TileBase tile) 
+        {
+            if(tile is RuleOverrideTile overrideTile)
+                tile = overrideTile.m_InstanceTile;
+ 
+            switch (neighbor) {
+                case TilingRuleOutput.Neighbor.This:
+                {
+                    return tile == this;
+                }
+                case TilingRuleOutput.Neighbor.NotThis:
+                {
+                    return tile != this;
+                } 
+                
+                case Neighbor.Null: return tile == null;
+                case Neighbor.NotNull: return tile != null;
+            }
+
+            return true;
+        }
     }
 }
